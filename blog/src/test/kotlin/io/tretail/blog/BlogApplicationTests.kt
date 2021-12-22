c10c341cce6a65fe6cbe13e7674db82e0ee0dc26
tre <tanguy.retail@lesfurets.com>
2021-12-22 12:26:26 +0100

    Initial commit, spring initializr

---
+++ blog/src/test/kotlin/io/tretail/blog/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.tretail.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
