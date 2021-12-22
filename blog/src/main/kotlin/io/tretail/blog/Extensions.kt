9ea7c81f94c71a6eded53fb514c84d84492c314d
tre <tanguy.retail@lesfurets.com>
2021-12-22 12:48:02 +0100

    Creating your own extensions

---
+++ blog/src/main/kotlin/io/tretail/blog/Extensions.kt
@@ -0,0 +1,31 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatterBuilder
+import java.time.temporal.ChronoField.DAY_OF_MONTH
+import java.util.Locale.ENGLISH
+
+fun LocalDateTime.format() = this.format(englishDateFormatter)
+
+private val daysLookup = (1..31).associate { it.toLong() to getOrdinal(it) }
+
+private val englishDateFormatter = DateTimeFormatterBuilder()
+    .appendPattern("yyyy-MM-dd")
+    .appendLiteral(" ")
+    .appendText(DAY_OF_MONTH, daysLookup)
+    .appendLiteral(" ")
+    .appendPattern("yyyy")
+    .toFormatter(ENGLISH)
+
+private fun getOrdinal(n: Int) = when {
+    n in 11..13 -> "${n}th"
+    n % 10 == 1 -> "${n}st"
+    n % 10 == 2 -> "${n}nd"
+    n % 10 == 3 -> "${n}rd"
+    else -> "${n}th"
+}
+
+fun String.toSlug() = lowercase(ENGLISH)
+    .replace("\n", " ")
+    .replace("[^a-z\\d\\s]".toRegex(), " ")
+    .split(" ")
+    .joinToString("-")
+    .replace("-+".toRegex(), "-")
